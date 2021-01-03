a57a7242065518b05c3a0ed62fd57a4ac22b82fc
thuandevk42 <thienmenhdevuong>
2021-01-03 20:09:50 +0700

    update

--- OOP/index.php
+++ OOP/index.php
@@ -10,12 +10,12 @@
 
 <?php
 
-class Person
-{
+// class Person
+// {
 
     /* public $name = "Le Van Thuan"; */
-    public $name;
-    public $id;
+//     public $name;
+//     public $id;
 
     /* private $name; */
     
@@ -24,10 +24,10 @@ class Person
 //         echo "Constructor Created.<br/>";
 //     }
 
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
+//     public function __construct($name)
+//     {
+//         $this->name = $name;
+//     }
 
 //     public function personName()
 //     {
@@ -39,39 +39,64 @@ class Person
 //         echo "Person name is: {$this->name}";
 //     }
 
-    public function setId($id) {
-        $this->id=$id;
-    }
+//     public function setId($id) {
+//         $this->id=$id;
+//     }
     
     /**
      * Hàm hủy là hàm tự động gọi sau khi đối tượng bị hủy, nó thường được sử dụng để giải phóng bộ nhớ chương trình
      */
-    public function __destruct(){
-        /*
-         * Hàm empty() trong php dùng để kiểm tra một biến nào đó có giá trị rỗng hoặc chưa được khởi tạo hay không
-         * hàm empty() để validate dữ liệu
-         * 
-         * */
-        if(!empty($this->id)){
-            echo "Saving person";
-        }
-    }
-}
+//     public function __destruct(){
+//         /*
+//          * Hàm empty() trong php dùng để kiểm tra một biến nào đó có giá trị rỗng hoặc chưa được khởi tạo hay không
+//          * hàm empty() để validate dữ liệu
+//          * 
+//          * */
+//         if(!empty($this->id)){
+//             echo "Saving person";
+//         }
+//     }
+// }
 
    /*  $personOne = new Person(); */
-        $personOne = new Person("Le Van Thuan");
+//         $personOne = new Person("Le Van Thuan");
     /* echo $personOne->name; */
     /* $personOne->name = "Le Van Thuan"; */
     /* $personOne->personName(); */
    /*  $personOne->personDetails(); */
-        $personOne->setId(12);
+//         $personOne->setId(12);
         /*
          * Hàm unset() sẽ loại bỏ một hoặc nhiều biến được truyền vào. 
          * Hàm unset() cũng có thể được sử dụng để loại bỏ một phần tử xác định trong mảng
          * Nếu một biến toàn cục bị unset() trong một hàm nào đó. biến đó sẽ chỉ bị loại bỏ trong phạm vi của hàm
          * Để xóa biến toàn cục trong hàm sử dụng mảng $GLOBALS
          * */
-        unset($personOne);
+//         unset($personOne);
+
+
+        class UserData {
+            public $user;
+            public $userId;
+            
+            public function __construct($user,$userId) {
+                $this->$user = $user;
+                $this->$userId = $userId;
+                
+                echo "User name is {$this->$user} and User ID is {$this->$userId}";
+            }
+            
+            public function __destruct(){
+                unset($this->user);
+                unset($this->userId);
+            }
+        }
+
+        $user = "Thuan";
+        $userId ="42";
+
+        $url = new UserData($user, $userId);
+
+
 
 ?>
 
