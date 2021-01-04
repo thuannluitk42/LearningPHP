8de77e530b37225d5fc86329277c8657e3d5d915
thuandevk42 <thienmenhdevuong>
2021-01-05 06:50:55 +0700

    array (key-value)

--- Array/index.php
+++ Array/index.php
@@ -8,19 +8,24 @@
 
 <?php
 
-$car = array(
-    "Suzuki",
-    "Yamaha",
-    "Honda"
-);
+/* Mang thuong  */
+// $car = array("Suzuki","Yamaha","Honda");
 
-echo $car[0]."<br/>";
+// echo $car[0]."<br/>";
 
-$length = count($car);
-for($i = 0; $i<$length; $i++){
-    echo $car[$i]."<br/>";
+// $length = count($car);
+// for($i = 0; $i<$length; $i++){
+//     echo $car[$i]."<br/>";
+// }
+
+/* Mang key - value */
+
+$age = array("Thuan"=>"23","Hoang"=>"24","Quynh"=>"24");
+
+foreach ($age as $key=>$value){
+    echo "Name = ".$key.", Age: = ".$value;
+    echo "<br/>";
 }
-
 
 ?>
 
