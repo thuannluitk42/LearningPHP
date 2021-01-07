64acdb1f6123a7fb0e15fcb6dc5e1b358c6db787
thuandevk42 <thienmenhdevuong>
2021-01-07 07:05:55 +0700

    init package CRUD_PHP_MySQLi

---
+++ CRUD_PHP_MySQLi/inc/footer.php
@@ -0,0 +1,9 @@
+
+</section>
+<footer class="footeroption">
+<h2> www.easylearningbd.com </h2>
+</footer>
+
+
+</body>
+</html>
---
+++ CRUD_PHP_MySQLi/inc/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD PHP for Beginners</title>
+
+<style>
+body {
+background-color: #e6e6e6;
+font-family: arial;
+font-size: 18px;
+line-height: 22px;
+margin: 0 auto;
+width: 800px;
+
+}
+.headeroption {
+	background: #3399ff url("img/php.png") no-repeat scroll 56px 18px;
+	height: 80px;
+	overflow: hidden;
+	padding-left: 170px;
+}
+
+.footeroption {
+background: #3399ff;
+height: 80px;
+overflow: hidden;
+
+}
+.content {
+background: #f2f2f2;
+border: 6px solid #3399ff;
+font-size: 16px;
+line-height: 22px;
+min-height: 450px;
+overflow: hidden; 
+margin-bottom: 8px;
+margin-top: 8px;
+
+}
+
+.footeroption h2 {
+	font-size: 25px;
+	text-align: center;
+}
+
+.tmain {
+width: 100%;
+border:1px solid;
+margin: 20px 0;
+
+}
+.tmain td {
+ text-align: center;
+}
+table.tmain tr:nth-child(2n) { background: #e6e6e6; height:30px; }
+
+
+</style>
+
+</head>
+<body>
+<header class="headeroption">
+<h2> CRUD PHP for Beginners </h2>
+</header>
+
+<section class="content">
