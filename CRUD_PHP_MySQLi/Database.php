fdfde9699e4efc55d370178ee6695d60957d23a1
thuandevk42 <thienmenhdevuong>
2021-01-07 07:32:03 +0700

    create database.php

--- CRUD_PHP_MySQLi/Database.php
+++ CRUD_PHP_MySQLi/Database.php
@@ -1,1 +1,21 @@
 <?php
+class Database {
+    public $host = DB_HOST;
+    public $user = DB_USER;
+    public $pass = DB_PASS;
+    public $dbname = DB_NAME;
+    
+    public $link;
+    public $error;
+    
+    private function connectDB() {
+        $this->link = new mysqli($this->host,$this->user,$this->pass,$this->dbname);
+        
+        if(!$this->link){
+            $this->error = "connection fail".$this->link->connect_error;
+        }else{
+            return FALSE;
+        }
+    }
+}
+?>
